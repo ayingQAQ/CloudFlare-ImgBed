69ed8ab6667d7146d9984fce693ad8a5a01e80bf
ying <140624509+ayingQAQ@users.noreply.github.com>
2026-09-08 07:23:54 +0800

    feat: back up HF direct uploads to Telegram

--- functions/upload/huggingface/commitUpload.js
+++ functions/upload/huggingface/commitUpload.js
@@ -7,6 +7,7 @@
 import { HuggingFaceAPI } from '../../utils/storage/huggingfaceAPI.js';
 import { fetchPageConfig, fetchUploadConfig } from '../../utils/sysConfig.js';
 import { getDatabase } from '../../utils/databaseAdapter.js';
+import { backupFileIdToTelegram } from '../../utils/storageTiering.js';
 import { moderateContent, endUpload, getUploadIp, getIPAddress, sanitizeUploadFolder, createResponse } from '../uploadTools.js';
 import { userAuthCheck, UnauthorizedResponse } from '../../utils/auth/userAuth.js';
 
@@ -133,6 +134,10 @@ export async function onRequestPost(context) {
             url
         };
         waitUntil(endUpload(uploadContext, fullId, metadata));
+
+        // HF 大文件是浏览器直传，不经过 /upload 的主文件请求。
+        // 在提交成功并持久化元数据后，从 HF 分片读取并异步备份到 Telegram。
+        waitUntil(backupFileIdToTelegram(context, fullId, 'huggingface'));
 
         // 返回成功响应
         const returnLink = `/file/${fullId}`;
